057d95de66a914c9253275c19293965d2778b3b3
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2017-11-14 18:42:49 +0100

    add basic UI components

--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Initial UI, more to come!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Query />
+                <Artists />
+            </View>
+        );
+    }
+}
+
+const Query = () => <TextInput placeholder='search for artist'/>;
+
+const Artists = () => (<ScrollView style={styles.artists}>
+        <Text>Artists...</Text>
+    </ScrollView>);
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ecf0f1',
+        alignItems: 'stretch',
+        paddingTop: 20,
+    },
+    artists: {
+        backgroundColor: 'white'
+    }
+});
+
