f032a32eae60e4f56117b6345533795cb630921c
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2017-11-17 15:01:12 +0100

    add styling and query name state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import {StyleSheet, Text, View, TextInput, ScrollView, ListView, FlatList} from 'react-native';
+
 
 export default class App extends React.Component {
+
+    state = {
+        artistsName : ''
+    };
+
     render() {
         return (
             <View style={styles.container}>
-                <Query />
-                <Artists />
+                <Query onChangeText={this.updateQueryArtist.bind(this)} />
+                <Artists text={this.state.artistsName} />
             </View>
         );
     }
+    updateQueryArtist (artistsName) {
+        this.setState({
+            ...this.state,
+            artistsName: artistsName
+        });
+    }
 }
 
-const Query = () => <TextInput placeholder="search for artist" />;
 
-const Artists = () => (
+const Query = ({onChangeText}) => <TextInput
+        onChangeText={onChangeText}
+        underlineColorAndroid='transparent'
+        style={styles.query}
+        keyboardType={'web-search'}
+        returnKeyType={'search'}
+        placeholder="search for artist"
+    />;
+
+const Artists = ({text}) => (
     <ScrollView style={styles.artists}>
-        <Text>Artists...</Text>
+        <Text>{`Artists:`}</Text>
+        <Text>{`...${text}...`}</Text>
     </ScrollView>
 );
 
@@ -25,6 +46,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#ecf0f1',
         alignItems: 'stretch',
         paddingTop: 20,
+    },
+    query: {
+        margin: 20,
+        padding: 10,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderRadius: 20,
+        borderColor: '#1db853',
+        fontSize: 24,
     },
     artists: {
         backgroundColor: 'white',
