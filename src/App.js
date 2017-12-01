ca4a9e618f20739b0eb9da7e0bcf5cf9141fd412
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2017-12-01 20:33:54 +0100

    split into Apollo wrapper and Main component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,27 @@
 import React from 'react';
-import {StyleSheet, Text, View, TextInput, ScrollView, ListView, FlatList} from 'react-native';
 
+import { ApolloClient, InMemoryCache } from 'apollo-client-preset';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+
+import Main from './Main';
 
 export default class App extends React.Component {
-
-    state = {
-        artistsName : ''
-    };
+    // Initialize Apollo Client with URL to our serve
+    createClient() {
+        return new ApolloClient({
+            link: createHttpLink({
+                uri: 'http://spotify-graphql-server.herokuapp.com/graphql',
+            }),
+            cache: new InMemoryCache(),
+        });
+    }
 
     render() {
         return (
-            <View style={styles.container}>
-                <Query onChangeText={this.updateQueryArtist.bind(this)} />
-                <Artists text={this.state.artistsName} />
-            </View>
+            <ApolloProvider client={this.createClient()}>
+                <Main />
+            </ApolloProvider>
         );
     }
-    updateQueryArtist (artistsName) {
-        this.setState({
-            ...this.state,
-            artistsName: artistsName
-        });
-    }
 }
-
-
-const Query = ({onChangeText}) => <TextInput
-        onChangeText={onChangeText}
-        underlineColorAndroid='transparent'
-        style={styles.query}
-        keyboardType={'web-search'}
-        returnKeyType={'search'}
-        placeholder="search for artist"
-    />;
-
-const Artists = ({text}) => (
-    <ScrollView style={styles.artists}>
-        <Text>{`Artists:`}</Text>
-        <Text>{`...${text}...`}</Text>
-    </ScrollView>
-);
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#ecf0f1',
-        alignItems: 'stretch',
-        paddingTop: 20,
-    },
-    query: {
-        margin: 20,
-        padding: 10,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderRadius: 20,
-        borderColor: '#1db853',
-        fontSize: 24,
-    },
-    artists: {
-        backgroundColor: 'white',
-    },
-});
---
+++ src/Main.js
@@ -0,0 +1,138 @@
+import { SearchBar } from 'react-native-elements';
+import React from 'react';
+import { ActivityIndicator, Image, StyleSheet, Text, View, ScrollView } from 'react-native';
+
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+let artistsList = [
+    {
+        name: 'Marilyn Manson',
+        id: 1,
+    },
+];
+
+export default class App extends React.Component {
+    state = {
+        artists: artistsList,
+        artistsByName: 'Marilyn Manson',
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Query onChangeText={this.updateQueryArtist.bind(this)} value={this.state.artistsByName} />
+                {!!this.state.artistsByName && <Artists byName={this.state.artistsByName} />}
+                {!this.state.artistsByName && <Text>Just search for something</Text>}
+            </View>
+        );
+    }
+
+    updateQueryArtist(artistsName = '') {
+        this.setState({
+            ...this.state,
+            artists: artistsList,
+            artistsByName: artistsName,
+        });
+    }
+}
+
+const Query = ({ onChangeText, value = '' }) => (
+    <SearchBar
+        value={value}
+        clearIcon={{ color: '#86939e', name: 'close' }}
+        icon={{ color: '#86939e', name: 'search' }}
+        round
+        lightTheme
+        onChangeText={onChangeText}
+        onClearText={onChangeText}
+        placeholder="Type artists name ..."
+    />
+);
+
+const ArtistsBase = ({ data = {} }) => {
+    let { artists = [], loading, error } = data;
+
+    //loading = true;
+    if (loading) {
+        return <ActivityIndicator />;
+    }
+    if (error) {
+        return <Text>{'' + error}</Text>;
+    }
+    return (
+        <ScrollView>
+            <View
+                style={
+                    {
+                        //... styles.artists,
+                        //flexDirection: 'row',
+                        //flexWrap: 'wrap'
+                    }
+                }>
+                {artists.filter(artist => artist.image).map(artist => <Artist key={artist.id} {...artist} />)}
+            </View>
+        </ScrollView>
+    );
+};
+
+const ArtistsQuery = gql`
+    query artists($byName: String!) {
+        artists: queryArtists(byName: $byName) {
+            name
+            image
+            id
+            albums {
+                name
+            }
+        }
+    }
+`;
+
+let config = {
+    options: ({ byName }) => ({
+        variables: {
+            byName,
+        },
+        pollInterval: 60 * 1000,
+    }),
+};
+
+const Artists = graphql(ArtistsQuery, config)(ArtistsBase);
+
+const Artist = ({ name, image, albums }) => (
+    <View>
+        <Text>{name}</Text>
+        <Text>
+            Albums:
+            {albums && albums.length}
+        </Text>
+        {!!image && <Image source={{ uri: image }} style={styles.artistImage} />}
+    </View>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ecf0f1',
+        alignItems: 'stretch',
+        paddingTop: 20,
+    },
+    query: {
+        margin: 20,
+        padding: 10,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderRadius: 20,
+        borderColor: '#1db853',
+        fontSize: 24,
+    },
+    artists: {
+        backgroundColor: 'white',
+    },
+    artist: {},
+    artistImage: {
+        height: 100,
+        width: 100,
+        margin: 10,
+    },
+});
